6a5a73f0b9e6403898e7516ed968ee0ec0b7ce3b
Nitzan Dahan <163737279+nitzandev@users.noreply.github.com>
2024-04-15 11:33:32 +0300

    Update user.test.ts

--- src/handlers/__tests__/user.test.ts
+++ src/handlers/__tests__/user.test.ts
@@ -2,7 +2,7 @@ import * as user from '../user'
 
 describe('user handler', () => {
   it('should create a new user', async () => {
-    const req = { body: { username: 'hello', password: 'hi' } }
+    const req = { body: { username: '', password: '' } }
     const res = { json({ token }) {
       expect(token).toBeTruthy()
     }}
